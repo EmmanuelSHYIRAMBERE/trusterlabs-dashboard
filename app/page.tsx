5889d857b18a33016a97a680fab450c3c5ba1ccc
EmmanuelSHYIRAMBERE <emashyirambere1@gmail.com>
2026-03-17 21:58:10 +0200

    fix: wrap LoginComponent in Suspense for useSearchParams

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { Suspense } from "react";
 import LoginComponent from "@/components/auth/LoginComponent";
 
 export default function Home() {
-  return <LoginComponent />;
+  return (
+    <Suspense>
+      <LoginComponent />
+    </Suspense>
+  );
 }
